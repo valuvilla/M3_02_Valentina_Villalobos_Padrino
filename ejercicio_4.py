d3e0cb49bf7756c49f863e626d9d5555f36ec263
valuvilla <valuvp@hotmail.com>
2022-11-15 19:04:35 +0100

    ejercicio4

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -1,3 +1,20 @@
 from ast import main
+import sys
 
-def error_4(*arh):
+
+def error_4(*args):
+    resultado=0
+    for numero in args:
+        try:
+            resultado=+numero
+            print(resultado)
+        except TypeError:
+            print("Se ha introducido un caracter que no es un numero")
+        finally:
+            return "--OPERACION EVALUADA--"
+            sys.exit()
+
+if __name__=="__main__":
+    print(error_4("2",10))
+
+
